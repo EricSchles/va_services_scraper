6e8862c2903f2a83341a63fef129664676ab7489
Eric Schles <ericschles@gmail.com>
2015-12-24 11:23:15 -0500

    updating

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import requests
 import lxml.html
 import pandas as pd
+import math
+from scipy import stats
+import scipy as sp
 
 class Scraper:
     def __init__(self):
@@ -33,7 +36,7 @@ class Scraper:
             locations = [elem.text_content() for elem in html.xpath('//td[@headers="loc"]')]
             phones = [elem.text_content() for elem in html.xpath('//td[@headers="ph"]')]
             descriptions = [elem.text_content() for elem in html.xpath('//td[@headers="srv"]//span')]
-            va_fac = va_facility.split("http://")[1].split(".va")[0]
+            va_fac = va_facility.split("http://")[1].split(".va")[0].split("www.")[1]
             for ind,val in enumerate(service_names):
                 tmp["service_name"] = self.clean(val)
                 tmp["link"] = self.clean(links[ind])
@@ -44,6 +47,34 @@ class Scraper:
                 df = df.append(tmp,ignore_index=True)
         df.to_csv("services_list.csv")
 
+class ServiceAnalyzer:
+    def __init__(self):
+        self.df = pd.read_csv("services_list.csv")
+        self.service_counts = self.services_count()
+        
+    def services_count(self):
+        counts = []
+        for i in self.df.index:
+            va_facility = self.df.ix[i]["va_facility"]
+            counts.append(va_facility)
+        counts = {elem:counts.count(elem) for elem in counts}
+        return counts
+    
+    def descriptive_statistics(self):
+        s = sp.array(self.service_counts.values())
+        n,min_max,mean,var,skew,kurt = stats.describe(s)
+        return {
+            "n":n,
+            "average":mean,
+            "std_dev":math.sqrt(var),
+            "skew":skew,
+            "kurtosis":kurt
+            }
+
+
 if __name__ == '__main__':
-    s = Scraper()
-    s.generate_services_csv()
+    #s = Scraper()
+    #s.generate_services_csv()
+    sa = ServiceAnalyzer()
+    print sa.services_count()
+    print sa.descriptive_statistics()
